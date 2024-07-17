e20c09dc55b2256e5485c8cb67ce07f1b6ee0955
berseabra <bernardo.seabra@cubos.io>
2024-07-17 11:38:43 -0300

    add middleware config

---
+++ src/app/api/middleware.tsx
@@ -0,0 +1,8 @@
+import { NextResponse } from "next/server";
+
+export const config = {
+  matcher: "/((?!_next/static|_next/image|favicon.ico).*)",
+};
+export function middleware() {
+  return NextResponse.next();
+}
